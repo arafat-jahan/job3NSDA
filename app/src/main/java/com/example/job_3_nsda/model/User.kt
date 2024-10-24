398875d7d054b7134b753657fd7cf0c8f08feeb6
arafat-jahan <ajh.official15@gmail.com>
2024-10-24 10:00:30 +0600

    new

---
+++ app/src/main/java/com/example/job_3_nsda/model/User.kt
@@ -0,0 +1,22 @@
+package com.example.job_3_nsda.model
+
+import com.google.firebase.database.PropertyName // Correct import
+
+
+data class User(
+    val userId: String,
+    @get:PropertyName("displayName")
+    @set:PropertyName("displayName")
+    var displayName: String = "",
+
+    @get:PropertyName("email")
+    @set:PropertyName("email")
+    var email: String = "",
+
+    @get:PropertyName("location")
+    @set:PropertyName("location")
+    var location: String = ""
+) {
+    // No-argument constructor
+    constructor() : this("", "", "")
+}
---
+++ app/src/main/java/com/example/job_3_nsda/view/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.job_3_nsda.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.job_3_nsda.R
+import com.example.job_3_nsda.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    lateinit var actionDrawerToggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomBar.setupWithNavController(navController)
+        binding.drawerNav.setupWithNavController(navController)
+
+        actionDrawerToggle = ActionBarDrawerToggle(
+            this, binding.drawerlayout,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        actionDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.drawerNav.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.logout -> {
+                    Firebase.auth.signOut()
+                    startActivity(
+                        Intent(this, LoginActivity::class.java)
+                    )
+                    finish()
+                }
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                }
+                R.id.friendsFragment -> {
+                    navController.navigate(R.id.friendsFragment)
+                }
+            }
+            true
+        }
+
+        binding.bottomBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.logout -> {
+                    Firebase.auth.signOut()
+                    startActivity(
+                        Intent(this, LoginActivity::class.java)
+                    )
+                    finish()
+                }
+                R.id.friendsFragment -> {
+                    navController.navigate(R.id.friendsFragment)
+                }
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+}
